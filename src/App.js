353d777079787fa1d0720eb068420c7aa3275fd3
bruno <brunocabral035@gmail.com>
2022-02-15 15:53:52 -0300

    API request done, Results div animated, media queries done, condition render to button depending on input done, clear button done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { GrCircleInformation } from "react-icons/gr";
 
 function App() {
 
-  useEffect(()=>{
-    // GET request using fetch with set headers
+  //Pagina carrega e já busca no servidor os indicadores
+  useEffect(() => {
     const headers = { 'Content-Type': 'application/json' }
-    fetch('http://localhost:3000/', { headers })
-        .then(response => response.json())
-        .then(data => console.log(data));
-  },[])
-    
+    fetch(`http://localhost:3000/indicadores`, { headers })
+      .then(response => response.json())
+      .then(data => {
+        setCdi(data[0].valor)
+        setIpca(data[1].valor)
+      });
+  }, [])
 
+  //Função que já busca os valores na API e define no resultado
+  function getData() {
+    const headers = { 'Content-Type': 'application/json' }
+    fetch(`http://localhost:3000/simulacoes/?tipoIndexacao=${indexa}&tipoRendimento=${Rendimento}`, { headers })
+      .then(response => response.json())
+      .then(data => {
+        setValorFB(data[0].valorFinalBruto);
+        setValorFL(data[0].valorFinalLiquido);
+        setValorP(data[0].valorPagoIR);
+        setValorT(data[0].valorTotalInvestido);
+        setAli(data[0].aliquotaIR);
+        setGL(data[0].ganhoLiquido);
+
+        setIsClicked(true);
+      });
+  }
+
+  //Variavel para verificar inputs
+  const [isFilled, setIsFilled] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
+  //Variaveis do form
   const [Rendimento, SetRendimento] = useState('bruto');
-  const [Aporte, setAporte] = useState();
-  const [Prazo, setPrazo] = useState();
-  const [ipca, setIpca] = useState();
-  const [AporteM, setAporteM] = useState();
-  const [Rentabilidade, setRentabilidade] = useState();
-  const [cdi, setCdi] = useState();
+  const [Aporte, setAporte] = useState('');
+  const [Prazo, setPrazo] = useState('');
+  const [ipca, setIpca] = useState('');
+  const [AporteM, setAporteM] = useState('');
+  const [Rentabilidade, setRentabilidade] = useState('');
+  const [cdi, setCdi] = useState('');
   const [indexa, setIndexa] = useState('pos');
 
+  //Variaveis de resultados
   const [valorFB, setValorFB] = useState(0);
   const [ValorP, setValorP] = useState(0);
   const [ValorT, setValorT] = useState(0);
@@ -28,19 +53,29 @@ function App() {
   const [ValorFL, setValorFL] = useState(0);
   const [GL, setGL] = useState(0);
 
-  function cleanInput(){
-    console.log('teste');
-  }
-  
-  function calc(){
-    console.log(Rendimento);
-    console.log(Aporte);
-    console.log(Prazo);
-    console.log(ipca);
-    console.log(AporteM);
-    console.log(Rentabilidade);
-    console.log(cdi);
-    console.log(indexa);
+  //UseEffect para verificar os inputs
+  useEffect(() => {
+    if ((Aporte !== '') && (AporteM !== '') && (Prazo !== '') && (Rentabilidade !== '')) {
+      setIsFilled(true);
+    }
+  }, [Aporte, AporteM, Rentabilidade, Prazo])
+
+  //Função para basicamente limpar os inputs
+  function cleanInput() {
+    setIndexa('pos');
+    SetRendimento('bruto');
+    setValorFB(0);
+    setValorFL(0);
+    setValorP(0);
+    setValorT(0);
+    setAli(0);
+    setGL(0);
+    setPrazo('');
+    setAporte('');
+    setAporteM('');
+    setRentabilidade('');
+    setIsFilled(false);
+    setIsClicked(false);
   }
 
   return (
@@ -58,17 +93,17 @@ function App() {
                 <GrCircleInformation />
               </div>
               <div className="option-box-1">
-                <input type="radio" name="question-1" value="bruto" id="bruto" defaultChecked onChange={(e)=>{SetRendimento(e.target.value)}} />
-                <label for="bruto" id="bru">Bruto</label>
-                <input type="radio" name="question-1" value="liquido" id="liquido" onChange={(e)=>{SetRendimento(e.target.value)}}/>
-                <label for="liquido" id="liq">Líquido</label>
+                <input type="radio" name="question-1" value="bruto" id="bruto" defaultChecked onChange={(e) => { SetRendimento(e.target.value) }} />
+                <label htmlFor="bruto" id="bru">Bruto</label>
+                <input type="radio" name="question-1" value="liquido" id="liquido" onChange={(e) => { SetRendimento(e.target.value) }} />
+                <label htmlFor="liquido" id="liq">Líquido</label>
               </div>
-              <label for="aporte-inicial">Aporte Inicial</label>
-              <input type="number" name="aporte-inicial" placeholder="R$ 0,00" onChange={(e)=>{setAporte(e.target.value)}}/>
-              <label for="prazo">Prazo (em meses)</label>
-              <input type="number" name="prazo" placeholder="0" onChange={(e)=>{setPrazo(e.target.value)}}/>
-              <label for="ipca">IPCA (ao ano)</label>
-              <input type="number" name="ipca" placeholder="0,00%" onChange={(e)=>{setIpca(e.target.value)}}/>
+              <label htmlFor="aporte-inicial">Aporte Inicial</label>
+              <input type="number" name="aporte-inicial" placeholder="R$ 0,00" onChange={(e) => { setAporte(e.target.value) }} value={Aporte} />
+              <label htmlFor="prazo">Prazo (em meses)</label>
+              <input type="number" name="prazo" placeholder="0" onChange={(e) => { setPrazo(e.target.value) }} value={Prazo} />
+              <label htmlFor="ipca">IPCA (ao ano)</label>
+              <input type="number" name="ipca" placeholder="0,00%" readOnly value={ipca} />
             </div>
             <div className="col-2">
               <div className="label-rendimento">
@@ -76,28 +111,29 @@ function App() {
                 <GrCircleInformation />
               </div>
               <div className="option-box-2">
-                <input type="radio" name="question-2" value="pre" id="pre" onChange={(e)=>{setIndexa(e.target.value)}}/>
-                <label for="pre" id="pr">Pré</label>
-                <input type="radio" name="question-2" value="pos" id="pos" defaultChecked onChange={(e)=>{setIndexa(e.target.value)}}/>
-                <label for="pos" id="po">Pós</label>
-                <input type="radio" name="question-2" value="fixado" id="fixado" onChange={(e)=>{setIndexa(e.target.value)}}/>
-                <label for="fixado" id="fix">Fixado</label>
+                <input type="radio" name="question-2" value="pre" id="pre" onChange={(e) => { setIndexa(e.target.value) }} />
+                <label htmlFor="pre" id="pr">Pré</label>
+                <input type="radio" name="question-2" value="pos" id="pos" defaultChecked onChange={(e) => { setIndexa(e.target.value) }} />
+                <label htmlFor="pos" id="po">Pós</label>
+                <input type="radio" name="question-2" value="ipca" id="fixado" onChange={(e) => { setIndexa(e.target.value) }} />
+                <label htmlFor="fixado" id="fix">Fixado</label>
               </div>
-              <label for="aporte-mensal">Aporte Mensal</label>
-              <input type="number" name="aporte-mensal" placeholder="R$ 0,00" onChange={(e)=>{setAporteM(e.target.value)}}/>
-              <label for="rentabilidade">Rentabilidade</label>
-              <input type="number" name="rentabilidade" placeholder="0%" onChange={(e)=>{setRentabilidade(e.target.value)}}/>
-              <label for="cdi">CDI (ao ano)</label>
-              <input type="number" name="cdi" placeholder="0,00%" onChange={(e)=>{setCdi(e.target.value)}}/>
+              <label htmlFor="aporte-mensal">Aporte Mensal</label>
+              <input type="number" name="aporte-mensal" placeholder="R$ 0,00" onChange={(e) => { setAporteM(e.target.value) }} value={AporteM} />
+              <label htmlFor="rentabilidade">Rentabilidade</label>
+              <input type="number" name="rentabilidade" placeholder="0%" onChange={(e) => { setRentabilidade(e.target.value) }} value={Rentabilidade} />
+              <label htmlFor="cdi">CDI (ao ano)</label>
+              <input type="number" name="cdi" placeholder="0,00%" readOnly value={cdi} />
             </div>
           </div>
           <div className="btns">
             <button className="clean" onClick={cleanInput}>Limpar campos</button>
-            <button className="sim" onClick={calc}>Simular</button>
+            {(isFilled) ? <button className="sim" onClick={getData}>Simular</button> : <button className="simClosed" onClick={() => { alert('Preencha todos os dados!'); }}>Simular</button>}
+
           </div>
         </div>
 
-        <div className="Resultados">
+        {(isClicked) ? <div className="Resultados">
           <h1>Resultados da Simulação</h1>
           <div className="cards">
             <div className="card">
@@ -105,7 +141,7 @@ function App() {
               <p>R$ {valorFB}</p>
             </div>
             <div className="card"><h2>Alíquota do IR</h2>
-              <p>{Ali} %</p></div>
+              <p>{Ali}%</p></div>
             <div className="card"><h2>Valor Pago em IR</h2>
               <p>R$ {ValorP}</p></div>
             <div className="card"><h2>Valor Final Líquido</h2>
@@ -115,11 +151,10 @@ function App() {
             <div className="card"><h2>Ganho Líquido</h2>
               <p>R$ {GL}</p></div>
           </div>
-        </div>
+        </div> : <span></span>  
+        }
 
       </div>
-
-
     </div>
   );
 }
